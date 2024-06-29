5a9e2472b160b09159379fea77a1e87588e211ad
LiamKohen <liamkohen@icloud.com>
2024-06-28 23:16:44 -0300

    Botones alcanza,sobra,no alcanza

--- Assets/ManagerScript.cs
+++ Assets/ManagerScript.cs
@@ -7,13 +7,22 @@ public class ManagerScript : MonoBehaviour
     public GameObject[] objetos1;
     public GameObject[] objetos2;
 
-     private int precioObjeto1;
+    private int precioObjeto1;
     private int precioObjeto2;
     private int dineroJugador;
 
     public Text objeto1Text;
     public Text objeto2Text;
     public Text dineroText;
+
+    public Button botonSobraYAlcanza;
+    public Button botonAlcanzaJusto;
+    public Button botonNoAlcanza;
+
+    private bool verificarSobraYAlcanza;
+    private bool verificarAlcanzaJusto;
+    private bool verificarNoAlcanza;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +30,7 @@ public class ManagerScript : MonoBehaviour
         RandomizarValores();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-     void DesactivarObjetos()
+    public void DesactivarObjetos()
     {
         foreach (GameObject obj in objetos1)
         {
@@ -51,8 +55,43 @@ public class ManagerScript : MonoBehaviour
 
         dineroJugador = Random.Range(1, 201);
 
-         objeto1Text.text = "Precio: $" + precioObjeto1;
+        objeto1Text.text = "Precio: $" + precioObjeto1;
         objeto2Text.text = "Precio: $" + precioObjeto2;
-        dineroText.text = "Dinero del jugador: $" + dineroJugador;
+            dineroText.text = "Dinero del jugador: $" + dineroJugador;
+    }
+    public void VerificarSobraYAlcanza()
+    {
+        if (dineroJugador > (precioObjeto1 + precioObjeto2))
+        {
+            Debug.Log("Correcto, sobra y alcanza");
+        }
+        else
+        {
+            Debug.Log("Incorrecto");
+        }
+    }
+
+    public void VerificarAlcanzaJusto()
+    {
+        if (dineroJugador == (precioObjeto1 + precioObjeto2))
+        {
+            Debug.Log("Correcto, alcanza justo");
+        }
+        else
+        {
+            Debug.Log("Incorrecto");
+        }
+    }
+
+    public void VerificarNoAlcanza()
+    {
+        if (dineroJugador < (precioObjeto1 + precioObjeto2))
+        {
+            Debug.Log("Correcto, no alcanza");
+        }
+        else
+        {
+            Debug.Log("Incorrecto");
+        }
     }
 }
